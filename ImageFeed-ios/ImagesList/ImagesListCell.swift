d0358e6f9f1ca29f7a66c73da794c2bb85c00322
Aleksandr <125129763+Kosmonur@users.noreply.github.com>
2023-05-09 17:54:07 +0300

    8 sprint for review

---
+++ ImageFeed-ios/ImagesList/ImagesListCell.swift
@@ -0,0 +1,35 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed-ios
+//
+//  Created by Александр Пичугин on 08.05.2023.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var gradientView: UIView!
+    private var gradientNotAdded = true
+    
+    func setupGradient() {
+        guard gradientNotAdded else {
+            return
+        }
+        let gradient = CAGradientLayer()
+        let colorTop = UIColor(named: "gradientTop")?.cgColor ?? UIColor.clear.cgColor
+        let colorBottom = UIColor(named: "gradientBottom")?.cgColor ?? UIColor.clear.cgColor
+        gradient.colors = [colorTop, colorBottom]
+        gradient.locations = [0, 1]
+        gradient.frame = CGRect(
+            x: 0,
+            y: 0,
+            width: self.bounds.size.width,
+            height: gradientView.frame.height)
+        gradientView.layer.addSublayer(gradient)
+        gradientNotAdded = false
+    }
+}
