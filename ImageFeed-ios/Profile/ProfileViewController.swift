b73412d54c2684f72a968e9522d440fa02ddf080
Aleksandr <125129763+Kosmonur@users.noreply.github.com>
2023-05-13 21:21:27 +0300

    added ProfileViewController

---
+++ ImageFeed-ios/Profile/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed-ios
+//
+//  Created by Александр Пичугин on 13.05.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var userPickImageView: UIImageView!
+    @IBOutlet private weak var logoutButton: UIButton!
+    @IBOutlet private weak var userNameLabel: UILabel!
+    @IBOutlet private weak var loginNameLabel: UILabel!
+    @IBOutlet private weak var profileTextLabel: UILabel!
+    @IBAction private func didTapLogout(_ sender: UIButton) {
+    }
+}
